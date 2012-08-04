0b301c47df23df39b703932587e1066b9a8b420a
Dzenan Ridjanovic <dzenanr@gmail.com>
2012-08-04 18:01:29 -0400

    remove files after the file renames

--- data/web/Categories.dart
+++
@@ -1,8 +0,0 @@
-
-class Categories extends Entities<Category> {
-  
-  Categories(Concept concept) : super.of(concept);
-  
-  Categories newEntities() => new Categories(concept);
-  
-}
--- data/web/WebLinks.dart
+++
@@ -1,8 +0,0 @@
-
-class WebLinks extends Entities<WebLink> {
-  
-  WebLinks(Concept concept) : super.of(concept);
-  
-  WebLinks newEntities() => new WebLinks(concept);
-  
-}
